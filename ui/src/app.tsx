896ead35b9c45b11866bbe7ba7f431d0e06293cf
Norman Qian <normanqian@normans-air.mynetworksettings.com>
2022-12-30 17:26:40 -0500

    ui work

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Urbit from '@urbit/http-api';
 import Tome from 'tome-db';
+import { Cascader, Button } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 
 const api = new Urbit('', '', window.desk);
 api.ship = window.ship;
@@ -11,12 +13,20 @@ console.log(api);
 const db = new Tome(api);
 console.log(db);
 const store = db.store();
+const floor = db.store();
 console.log(store);
+console.log(floor);
 const appPreferencesStash = store.create('app.preferences');
+const floorstash = floor.create('floorstash');
+console.log(floorstash);
 console.log(store);
 await appPreferencesStash.set('alice', 'bob');
 await appPreferencesStash.set('charlie', 'david');
 
+await floorstash.set('1', '2');
+
+const floorvalue = await floorstash.get('1'); //check console logging
+console.log(floorvalue);
 
 /*
 
@@ -72,10 +82,142 @@ resp = await localStash.all();
 console.log(resp);
 
 export function App() {
+
+  const headers = [api.ship, 'Desk', 'Stash'];
+
+  const buttons = [<Button onClick={() => {const store = db.store()}}>New Store</Button>, 
+  <Button onClick={() =>{const createdStash = store.create(prompt("Enter Stash Name"))}}>Add Stash</Button>, 
+  <Button onClick={() => console.log("GHI")}>Add Field</Button>];
+
+  /*
+
+const db = new Tome(api);
+console.log(db);
+const store = db.store();
+
+var x = prompt("Enter Stash Name")
+
+use array[]?
+
+const createdStash = store.create(x)}
+
+later in the program im gonna need to reference the stash
+
+
+const appPreferencesStash = store.create('app.preferences');
+const floorstash = floor.create('floorstash');
+
+await appPreferencesStash.set('alice', 'bob');
+await appPreferencesStash.set('charlie', 'david');
+
+await floorstash.set('1', '2');
+
+const floorvalue = await floorstash.get('1'); //check console logging
+console.log(floorvalue);
+
+  */
+
+  const options = [
+    {
+      "label": "desk",  //desk
+      "value": 1,
+      "children": [   //a variable amount of children based on how many stashes are in the store, label is the stashnames
+        {
+          "label": "stashname", //stash
+          "value": 2
+        },
+        {
+          "label": "stashname",  //stash, variable amount of children based on how many kv pairs are inside
+          "value": 3,
+          "children": [
+            {
+              "label": "key: value",  //kv pair or field
+              "value": 4
+            },
+            {
+              "label": "key: value",
+              "value": 5
+            },
+            {
+              "label": "key: value",
+              "value": 6
+            },
+          ]
+        },
+      ]
+    }
+  ]
+  
   return (
-    <main className="flex items-center justify-center min-h-screen">
-    </main>
+    <div style={{
+      display: 'block', width: 600, paddingLeft: 30
+    }}>
+      <h4>Tome DB</h4>
+      <Cascader
+        inline
+        getChildren={node => {
+          return fetchNodes(node.id);
+        }}
+        renderMenu={(children, menu, parentNode, layer) => {
+          return (
+              <div>                   
+                <div
+                  style={{
+                    background: '#154c94',
+                    padding: '4px 10px',
+                    color: ' #fff',
+                    textAlign: 'center'
+                  }}
+                >
+                {headers[layer]}
+              </div>
+              
+              {buttons[layer]}
+              
+              {menu}
+            </div>
+          );
+        }}
+        block
+        searchable={false}
+        menuHeight="auto" 
+        menuWidth={180}
+        placeholder="TomeDB Placeholder Text"
+        data={options}
+      />
+    </div>
   );
 }
 
-//test commit 1
+/*
+
+show ship
+show store (empty)
+show stash (empty)
+show active desks
+
+onclick desk
+show store and store contents
+
+onclick stash
+show stash and contents
+
+onclick newstore
+if store exists, print "already created"
+else create store and print "store created"
+
+onclick add stash
+prompt for stash name
+create stash with name
+trigger onclick desk (to refresh store contents to show stash)
+
+onclick add field
+prompt for new field
+create key value entry
+trigger onclick stash (to refresh stash contents to show key values)
+
+showchildren/formatting is key
+
+there should be a method to show all stashes in a store
+
+*/
